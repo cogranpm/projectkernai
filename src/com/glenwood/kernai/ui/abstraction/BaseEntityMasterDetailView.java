ee0ad6682399ebe7ae043b86f96d1ff3bb538aad
Paul Martin <cogranpm@gmail.com>
2017-02-13 19:06:00 -0600

    implementing base master detail view

--- src/com/glenwood/kernai/ui/abstraction/BaseEntityMasterDetailView.java
+++ src/com/glenwood/kernai/ui/abstraction/BaseEntityMasterDetailView.java
@@ -4,13 +4,27 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.eclipse.core.databinding.DataBindingContext;
+import org.eclipse.core.databinding.observable.list.WritableList;
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.ActionContributionItem;
 import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.action.ToolBarManager;
+import org.eclipse.jface.viewers.DoubleClickEvent;
+import org.eclipse.jface.viewers.IDoubleClickListener;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.KeyEvent;
+import org.eclipse.swt.events.KeyListener;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.ToolBar;
 import org.eclipse.swt.widgets.ToolItem;
 
 import com.glenwood.kernai.data.abstractions.BaseEntity;
+import com.glenwood.kernai.ui.ApplicationData;
 
 public class BaseEntityMasterDetailView<T extends BaseEntity, P extends BaseEntity> extends Composite implements IEntityMasterDetailView<T, P> {
 
@@ -23,9 +37,185 @@ public class BaseEntityMasterDetailView<T extends BaseEntity, P extends BaseEnti
 	protected DataBindingContext ctx;
 	protected TableViewer listViewer;
 	protected Table listTable;
+	protected Composite headerContainer;
+	protected Composite listContainer;
 	
-	public BaseEntityMasterDetailView(Composite parent, int style) {
+	protected IEntityMasterDetailPresenter<T, P> presenter;
+	protected IMasterDetailViewModel<T, P> model;
+	protected WritableList<T> input;
+	
+	public BaseEntityMasterDetailView(Composite parent, int style, P parentEntity) {
 		super(parent, style);
+		setupModelAndPresenter(parentEntity);
+		init();
+	}
+	
+	
+	public IEntityMasterDetailPresenter<T, P> getPresenter() {
+		return presenter;
+	}
+
+	protected void setupModelAndPresenter(P parentEntity)
+	{
+		
+	}
+	
+	protected void setupListColumns()
+	{
+		
+	}
+	
+	protected void initDataBindings()
+	{
+		
+	}
+
+	
+	protected void init()
+	{
+		this.createActions();
+		
+		headerContainer = new Composite(this, SWT.NONE);
+		GridLayout headerLayout = new GridLayout(1, false);
+		headerLayout.marginHeight = 0;
+		headerLayout.marginWidth = 0;
+		headerContainer.setLayout(headerLayout);
+		
+
+		ToolBar actionsBar = new ToolBar(headerContainer, SWT.NONE);
+		ToolBarManager toolBarManager = new ToolBarManager(actionsBar);
+		ActionContributionItem newAction = new ActionContributionItem(this.actionMap.get(NEW_ACTION_KEY));
+		
+		toolBarManager.add(newAction);
+		newAction = new ActionContributionItem(this.actionMap.get(DELETE_ACTION_KEY));
+		toolBarManager.add(newAction);
+		newAction = new ActionContributionItem(this.actionMap.get(EDIT_ACTION_KEY));
+		toolBarManager.add(newAction);
+		toolBarManager.update(true);
+		
+
+		ToolBar toolbar = toolBarManager.getControl();
+		toolbar.addKeyListener(new KeyListener() {
+			
+			@Override
+			public void keyReleased(KeyEvent e) {
+				// TODO Auto-generated method stub
+				System.out.println("farook");
+			}
+			
+			@Override
+			public void keyPressed(KeyEvent e) {
+				// TODO Auto-generated method stub
+				System.out.println("farook");				
+			}
+		});
+		
+		if (toolbar != null )
+		{
+			for(ToolItem toolItem : toolbar.getItems())
+			{
+				ActionContributionItem actionItem = (ActionContributionItem)toolItem.getData();
+				toolItemMap.put(actionItem.getAction().getActionDefinitionId(), toolItem);
+			}
+		}
+		
+	//	headerLabel.setText("List Items");
+		actionsBar.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false, 1, 1 ));
+		//headerLabel.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1 ));
+		
+		listContainer = new Composite(this, SWT.NONE);
+		listViewer = new TableViewer(listContainer, SWT.SINGLE | SWT.H_SCROLL | SWT.V_SCROLL | SWT.FULL_SELECTION | SWT.BORDER);
+		listTable = listViewer.getTable();
+		listTable.setHeaderVisible(true);
+		listTable.setLinesVisible(true);
+		this.setupListColumns();
+		listViewer.addDoubleClickListener(new IDoubleClickListener() {
+			
+			@Override
+			public void doubleClick(DoubleClickEvent event) {
+				T selectedItem = getSelection();
+				if(selectedItem != null)
+				{
+					presenter.editModel(selectedItem);
+				}
+				
+			}
+		});
+		
+		this.ctx = new DataBindingContext();
+		
+		initDataBindings();
+		this.presenter.loadItems();
+		GridLayout mainLayout = new GridLayout(1, false);
+		mainLayout.verticalSpacing = SWT.FILL;
+		mainLayout.horizontalSpacing = SWT.FILL;
+		mainLayout.marginHeight = 0;
+		mainLayout.marginWidth = 0;
+		headerContainer.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false, 1, 1 ));
+		listContainer.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1 ));
+		this.setLayout(mainLayout);
+	}
+	
+	private void createActions()
+	{
+		 IAction newAction = new Action() {
+			@Override 
+			public void run() {
+				presenter.addModel();
+			}
+		 };
+		 newAction.setEnabled(true);
+		 newAction.setAccelerator(SWT.ALT | 'j');
+		 newAction.setImageDescriptor(ApplicationData.instance().getImageRegistry().getDescriptor(ApplicationData.IMAGE_ADD_SMALL));
+		 newAction.setActionDefinitionId(ID_PREFIX + NEW_ACTION_KEY);
+		 this.actionMap.put(NEW_ACTION_KEY, newAction);
+		 
+		 IAction deleteAction = new Action(){
+			 @Override
+			 public void run(){
+				 T selectedItem = getSelection();
+				 if (selectedItem != null)
+				 {
+					 if(ApplicationData.instance().confirmDelete(getShell()) == true)
+					 {
+						 presenter.deleteModel(selectedItem);
+					 }
+				 }
+			 }
+		 };
+		 deleteAction.setEnabled(false);
+		 deleteAction.setImageDescriptor(ApplicationData.instance().getImageRegistry().getDescriptor(ApplicationData.IMAGE_CANCEL_SMALL));
+		 deleteAction.setDisabledImageDescriptor(ApplicationData.instance().getImageRegistry().getDescriptor(ApplicationData.IMAGE_CANCEL_DISABLED_SMALL));
+		 deleteAction.setActionDefinitionId(ID_PREFIX + DELETE_ACTION_KEY);
+		 this.actionMap.put(DELETE_ACTION_KEY, deleteAction);
+		 
+		 IAction editAction = new Action(){
+			 @Override
+			 public void run(){
+				 T selectedItem = getSelection();
+				 if (selectedItem != null)
+				 {
+					 presenter.editModel(selectedItem);
+				 }
+			 }
+		 };
+		 editAction.setEnabled(false);
+		 editAction.setImageDescriptor(ApplicationData.instance().getImageRegistry().getDescriptor(ApplicationData.IMAGE_EDIT_SMALL));
+		 editAction.setDisabledImageDescriptor(ApplicationData.instance().getImageRegistry().getDescriptor(ApplicationData.IMAGE_EDIT_DISABLED_SMALL));
+		 editAction.setActionDefinitionId(ID_PREFIX + EDIT_ACTION_KEY);
+		 this.actionMap.put(EDIT_ACTION_KEY, editAction);
+	}
+
+	
+	protected T getSelection()
+	{
+		T item = null;
+		IStructuredSelection selection = (IStructuredSelection)listViewer.getSelection();
+		if(selection != null)
+		{
+			item = (T)selection.getFirstElement();
+		}
+		return item;
 	}
 
 	@Override
--- src/com/glenwood/kernai/ui/view/ListDetailMasterDetailView.java
+++ src/com/glenwood/kernai/ui/view/ListDetailMasterDetailView.java
@@ -41,85 +41,35 @@ import com.glenwood.kernai.data.entity.ListDetail;
 import com.glenwood.kernai.data.entity.ListHeader;
 import com.glenwood.kernai.ui.ApplicationData;
 import com.glenwood.kernai.ui.abstraction.BaseEntityMasterDetailView;
+import com.glenwood.kernai.ui.abstraction.IEntityMasterDetailPresenter;
 import com.glenwood.kernai.ui.abstraction.IEntityMasterDetailView;
 import com.glenwood.kernai.ui.presenter.ListDetailViewPresenter;
 import com.glenwood.kernai.ui.viewmodel.ListDetailViewModel;
 
-public class ListDetailMasterDetailView extends BaseEntityMasterDetailView<ListDetail, ListHeader> {
+public final class ListDetailMasterDetailView extends BaseEntityMasterDetailView<ListDetail, ListHeader> {
 	
-	private ListDetailViewPresenter presenter;
-	private ListDetailViewModel model;
-
-	private WritableList<ListDetail> input;
-
-	
-	
-	public ListDetailViewPresenter getPresenter() {
-		return presenter;
+	private  ListDetailMasterDetailView(Composite parent, int style) {
+		this(parent, style, null);
 	}
-
+	
 	
 	public ListDetailMasterDetailView(Composite parent, int style, ListHeader listHeader)
 	{
-		super(parent, SWT.BORDER);
-		this.model = new ListDetailViewModel(listHeader);
-		this.presenter = new ListDetailViewPresenter(this, model);
-		this.createActions();
-		
-		Composite headerContainer = new Composite(this, SWT.NONE);
-		GridLayout headerLayout = new GridLayout(1, false);
-		headerLayout.marginHeight = 0;
-		headerLayout.marginWidth = 0;
-		headerContainer.setLayout(headerLayout);
-		
-		//Label headerLabel = new Label(headerContainer, SWT.NONE);
-		ToolBar actionsBar = new ToolBar(headerContainer, SWT.NONE);
-		ToolBarManager toolBarManager = new ToolBarManager(actionsBar);
-		ActionContributionItem newAction = new ActionContributionItem(this.actionMap.get(NEW_ACTION_KEY));
-//		newAction.getAction().setAccelerator(SWT.ALT | 'j');
-		
-		toolBarManager.add(newAction);
-		newAction = new ActionContributionItem(this.actionMap.get(DELETE_ACTION_KEY));
-		toolBarManager.add(newAction);
-		newAction = new ActionContributionItem(this.actionMap.get(EDIT_ACTION_KEY));
-		toolBarManager.add(newAction);
-		toolBarManager.update(true);
-		
+		super(parent, SWT.BORDER, listHeader);
+	}
+	
+	@Override
+	protected void setupModelAndPresenter(ListHeader parent)
+	{
+		this.model = new ListDetailViewModel(parent);
+		this.presenter = new ListDetailViewPresenter(this, (ListDetailViewModel) model);
+	}
 
-		ToolBar toolbar = toolBarManager.getControl();
-		toolbar.addKeyListener(new KeyListener() {
-			
-			@Override
-			public void keyReleased(KeyEvent e) {
-				// TODO Auto-generated method stub
-				System.out.println("farook");
-			}
-			
-			@Override
-			public void keyPressed(KeyEvent e) {
-				// TODO Auto-generated method stub
-				System.out.println("farook");				
-			}
-		});
+
+	@Override
+	protected void setupListColumns() {
+		super.setupListColumns();
 		
-		if (toolbar != null )
-		{
-			for(ToolItem toolItem : toolbar.getItems())
-			{
-				ActionContributionItem actionItem = (ActionContributionItem)toolItem.getData();
-				toolItemMap.put(actionItem.getAction().getActionDefinitionId(), toolItem);
-			}
-		}
-		
-	//	headerLabel.setText("List Items");
-		actionsBar.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false, 1, 1 ));
-		//headerLabel.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1 ));
-		
-		Composite listContainer = new Composite(this, SWT.NONE);
-		listViewer = new TableViewer(listContainer, SWT.SINGLE | SWT.H_SCROLL | SWT.V_SCROLL | SWT.FULL_SELECTION | SWT.BORDER);
-		listTable = listViewer.getTable();
-		listTable.setHeaderVisible(true);
-		listTable.setLinesVisible(true);
 		TableViewerColumn keyColumn = new TableViewerColumn(listViewer, SWT.LEFT);
 		keyColumn.getColumn().setText("Key");
 		keyColumn.getColumn().setResizable(false);
@@ -132,120 +82,18 @@ public class ListDetailMasterDetailView extends BaseEntityMasterDetailView<ListD
 		listContainer.setLayout(tableLayout);
 		tableLayout.setColumnData(keyColumn.getColumn(), new ColumnWeightData(50));
 		tableLayout.setColumnData(labelColumn.getColumn(), new ColumnWeightData(50));
-		
-		listViewer.addDoubleClickListener(new IDoubleClickListener() {
-			
-			@Override
-			public void doubleClick(DoubleClickEvent event) {
-				ListDetail selectedItem = getSelection();
-				if(selectedItem != null)
-				{
-					presenter.editModel(selectedItem);
-				}
-				
-			}
-		});
-		
-		this.ctx = new DataBindingContext();
-		initDataBindings();
-		this.presenter.loadItems();
-		GridLayout mainLayout = new GridLayout(1, false);
-		mainLayout.verticalSpacing = SWT.FILL;
-		mainLayout.horizontalSpacing = SWT.FILL;
-		mainLayout.marginHeight = 0;
-		mainLayout.marginWidth = 0;
-		headerContainer.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false, 1, 1 ));
-		listContainer.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1 ));
-		this.setLayout(mainLayout);
-		
-		this.addKeyListener(new KeyListener() {
-			
-			@Override
-			public void keyReleased(KeyEvent e) {
-				// TODO Auto-generated method stub
-				System.out.println("hi");
-			}
-			
-			@Override
-			public void keyPressed(KeyEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-		});
 	}
-	
-	private ListDetail getSelection()
-	{
-		ListDetail item = null;
-		IStructuredSelection selection = (IStructuredSelection)listViewer.getSelection();
-		if(selection != null)
-		{
-			item = (ListDetail)selection.getFirstElement();
-		}
-		return item;
-	}
-	
-	private void createActions()
-	{
-		 IAction newAction = new Action() {
-			@Override 
-			public void run() {
-				presenter.addModel();
-			}
-		 };
-		 newAction.setEnabled(true);
-		 newAction.setAccelerator(SWT.ALT | 'j');
-		 newAction.setImageDescriptor(ApplicationData.instance().getImageRegistry().getDescriptor(ApplicationData.IMAGE_ADD_SMALL));
-		 newAction.setActionDefinitionId(ID_PREFIX + NEW_ACTION_KEY);
-		 this.actionMap.put(NEW_ACTION_KEY, newAction);
-		 
-		 IAction deleteAction = new Action(){
-			 @Override
-			 public void run(){
-				 ListDetail selectedItem = getSelection();
-				 if (selectedItem != null)
-				 {
-					 if(ApplicationData.instance().confirmDelete(getShell()) == true)
-					 {
-						 presenter.deleteModel(selectedItem);
-					 }
-				 }
-			 }
-		 };
-		 deleteAction.setEnabled(false);
-		 deleteAction.setImageDescriptor(ApplicationData.instance().getImageRegistry().getDescriptor(ApplicationData.IMAGE_CANCEL_SMALL));
-		 deleteAction.setDisabledImageDescriptor(ApplicationData.instance().getImageRegistry().getDescriptor(ApplicationData.IMAGE_CANCEL_DISABLED_SMALL));
-		 deleteAction.setActionDefinitionId(ID_PREFIX + DELETE_ACTION_KEY);
-		 this.actionMap.put(DELETE_ACTION_KEY, deleteAction);
-		 
-		 IAction editAction = new Action(){
-			 @Override
-			 public void run(){
-				 ListDetail selectedItem = getSelection();
-				 if (selectedItem != null)
-				 {
-					 presenter.editModel(selectedItem);
-				 }
-			 }
-		 };
-		 editAction.setEnabled(false);
-		 editAction.setImageDescriptor(ApplicationData.instance().getImageRegistry().getDescriptor(ApplicationData.IMAGE_EDIT_SMALL));
-		 editAction.setDisabledImageDescriptor(ApplicationData.instance().getImageRegistry().getDescriptor(ApplicationData.IMAGE_EDIT_DISABLED_SMALL));
-		 editAction.setActionDefinitionId(ID_PREFIX + EDIT_ACTION_KEY);
-		 this.actionMap.put(EDIT_ACTION_KEY, editAction);
-	}
-	
-	
-	
-	private  ListDetailMasterDetailView(Composite parent, int style) {
-		this(parent, style, null);
 
-	}
+	
+	
+	
+	
+
 	
 	public void showAddEdit(Boolean adding)
 	{
 		ListDetailModalView modalView = new ListDetailModalView(getShell());
-		modalView.setModel(model);
+		modalView.setModel((ListDetailViewModel) model);
 		if(modalView.open() == Window.OK)
 		{
 			this.presenter.saveModel();
@@ -266,7 +114,8 @@ public class ListDetailMasterDetailView extends BaseEntityMasterDetailView<ListD
 		listViewer.setInput(input);	
 	}
 	
-	private void initDataBindings()
+	@Override
+	protected void initDataBindings()
 	{
         ObservableListContentProvider detailContentProvider = new ObservableListContentProvider();
         listViewer.setContentProvider(detailContentProvider);
