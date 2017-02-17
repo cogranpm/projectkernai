e1d2ddc52e91437ed424cd1407331bf90f0110ac
Paul Martin <mussym@live.com>
2017-02-17 14:30:58 -0600

    implementin the master property view

--- src/com/glenwood/kernai/ui/view/MasterPropertyView.java
+++ src/com/glenwood/kernai/ui/view/MasterPropertyView.java
@@ -1,13 +1,26 @@
 package com.glenwood.kernai.ui.view;
 
+import org.eclipse.jface.layout.TableColumnLayout;
+import org.eclipse.jface.viewers.CellEditor;
+import org.eclipse.jface.viewers.ColumnWeightData;
+import org.eclipse.jface.viewers.EditingSupport;
+import org.eclipse.jface.viewers.TableViewerColumn;
+import org.eclipse.jface.viewers.TextCellEditor;
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
 
+import com.glenwood.kernai.data.entity.ListHeader;
 import com.glenwood.kernai.data.entity.MasterProperty;
 import com.glenwood.kernai.ui.abstraction.BaseEntityView;
 import com.glenwood.kernai.ui.presenter.MasterPropertyViewPresenter;
 import com.glenwood.kernai.ui.viewmodel.MasterPropertyViewModel;
 
 public class MasterPropertyView extends BaseEntityView<MasterProperty> {
+	
+	Label lblName;
+	Text txtName;
 
 	public MasterPropertyView(Composite parent, int style) {
 		super(parent, style);
@@ -23,11 +36,24 @@ public class MasterPropertyView extends BaseEntityView<MasterProperty> {
 	@Override
 	protected void setupListColumns() {
 		super.setupListColumns();
+		TableViewerColumn nameColumn = new TableViewerColumn(listViewer, SWT.LEFT);
+		nameColumn.getColumn().setText("Name");
+		nameColumn.getColumn().setResizable(false);
+		nameColumn.getColumn().setMoveable(false);
+//		nameColumn.getColumn().addSelectionListener(this.getSelectionAdapter(nameColumn.getColumn(), 0));
+		TableColumnLayout tableLayout = new TableColumnLayout();
+		listContainer.setLayout(tableLayout);
+		tableLayout.setColumnData(nameColumn.getColumn(), new ColumnWeightData(100));
 	}
 	
 	@Override
 	protected void setupEditingContainer() {
 		super.setupEditingContainer();
+		lblName = new Label(editMaster, SWT.NONE);
+		lblName.setText("Name");
+		txtName = viewHelper.getTextEditor(editMaster);
+		viewHelper.layoutEditLabel(lblName);
+		viewHelper.layoutEditEditor(txtName);
 	}
 	
 	@Override
